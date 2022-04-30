2e1c1e6f579f2a4d16a2096f68b61af71d20d836
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-04-30 06:25:31 +0530

    #include<stdio.h> int main() {     int n,a[200],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&a[i]);     }     for(i=0;i<n;i++)     {         if(i%2==0 && a[i]%2!=0)         {             c=1;         }     }     if(c==1)     {         printf("False");     }     else     {         printf("True");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,a[200],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==0 && a[i]%2!=0)
+        {
+            c=1;
+        }
+    }
+    if(c==1)
+    {
+        printf("False");
+    }
+    else
+    {
+        printf("True");
+    }
+}
