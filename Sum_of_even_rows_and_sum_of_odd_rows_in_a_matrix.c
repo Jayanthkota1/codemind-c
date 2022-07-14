4ff01efcfb5aec569e1df4ed2a04e94a80d6816f
Jayanthkota1 <98029765+Jayanthkota1@users.noreply.github.com>
2022-07-14 12:07:57 +0530

    #include<stdio.h> int main() {     int r,p,a[20][20],b,i,j,e=0,o=0;     scanf("%d%d",&r,&p);     for(i=0;i<r;i++)     {         for(j=0;j<p;j++)         {             scanf("%d",&a[i][j]);         }     }     for(i=0;i<r;i++)     {        b=0;         for(j=0;j<p;j++)         {         if(i%2==0)         {             e=e+a[i][j];         }         else         {             o=o+a[i][j];         }         }     }     printf("%d %d",e,o);
    }

--- Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
+++ Sum_of_even_rows_and_sum_of_odd_rows_in_a_matrix.c
@@ -1,28 +1,30 @@
 #include<stdio.h>
 int main()
 {
-    int c,r,a[20][20],i,j,e=0,o=0;
-    scanf("%d%d",&r,&c);
+    int r,p,a[20][20],b,i,j,e=0,o=0;
+    scanf("%d%d",&r,&p);
     for(i=0;i<r;i++)
     {
-        for(j=0;j<c;j++)
+        for(j=0;j<p;j++)
         {
             scanf("%d",&a[i][j]);
         }
     }
     for(i=0;i<r;i++)
     {
-        for(j=0;j<c;j++)
-       {
-           if(i%2==0)
-           {
-                e=e+a[i][j];
-           }
-           else
-           {
-               o=o+a[i][j];
-           }
+       b=0;
+        for(j=0;j<p;j++)
+        {
+        if(i%2==0)
+        {
+            e=e+a[i][j];
+        }
+        else
+        {
+            o=o+a[i][j];
+        }
         }
     }
     printf("%d %d",e,o);
+    
 }
